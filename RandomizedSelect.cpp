ab22c50dcbfc7e59c1af8b4b1d6a5376c0fc5cf2
一碗风 <1663425531@qq.com>
2016-09-24 18:18:59 +0800

    401 2016-9-24

--- RandomizedSelect.cpp
+++ RandomizedSelect.cpp
@@ -2,13 +2,16 @@
 #include <cmath> 
 #include <stdlib.h>
 #include <ctime>
+#include <algorithm>
 //线性时间查找
 
 
 using namespace std;
 
 int a[100];
-
+///////////////////////////////////
+///////一般来说此法时间复杂度O(N)//
+///////////////////////////////////
 template<class T_> 
 void Swap(T_ &a,T_ &b) {
 	T_ it = a;
@@ -29,7 +32,7 @@ int Partition(T_ a[],int s,int e) {
 	}
 	a[s] = a[j];
 	a[j] = x;
-	return j;//返回中间位置 
+	return j;//返回中间位置(严格来说这并不是中间数)
 }
 template<class T_> 
 int RandomizedPartition(T_ a[], int s,int e) {
@@ -53,6 +56,38 @@ T_ RandomizedSelect(T_ a[],int s,int e,int k) {
 	
 }
 
+///////////////////////////////////
+///////此法最差时间复杂度O(N)//////
+///////////////////////////////////
+
+template<class T_>
+bool cmp(T_ a,T_ b) {
+	if(a<=b)
+		return true;
+	return false;
+}
+
+template<class T_>
+T_ Select(T_　a[],int s,int e,int k) {
+	if(e-s<75) {
+		sort(a,a+e,cmp<T_>); //某简单算法将a排序 
+		return a[s+k-1];
+	}
+	for(int i=0;i<=(e-s-4)/5;++i) {
+		//将a[s+5*i]至a[s+5*i+4]的第三小元素与a[s+i]
+		//Swap<T_>(a[s+i],); 
+	}
+	//交换位置，找到中位数的中位数
+	T_ x = Select<T_>(a,s,s+(e-s-4)/5,(e-s-4)/10);
+	int i = Partition<T_>(a,s,e,x),j=i-s+1;
+	if(k<=j) 
+		return Select<T_>(a,s,i,k);
+	else
+		return Select<T_>(a,i+1,e,k-j);
+	
+} 
+
+
 int main() {
 	
 	for(int i=0;i<100;++i) {
@@ -62,8 +97,9 @@ int main() {
 	int pp=0;
 	cin>>pp;
 	int j = RandomizedSelect<int>(a,0,99,pp);
-	cout<<j;
-	
+	int c = Select<int>(a,0,99,pp);
+	cout<<j<<endl<<c;
+	cin>>pp;
 	
 	
 	return 0;
