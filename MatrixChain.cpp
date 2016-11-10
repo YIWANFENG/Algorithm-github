e0657f5b5123b680b3787ed6dfe71f44bae06914
一碗风 <1663425531@qq.com>
2016-11-10 11:22:37 +0800

    Add some details

--- MatrixChain.cpp
+++ MatrixChain.cpp
@@ -57,6 +57,39 @@ void Traceback(int i,int j) {
 	cout<<" and A "<<(s[i][j]+1)<<","<<j<<endl;
 } 
 
+#include <iostream>
+
+
+using namespace std;
+
+
+const int n = 5;
+int m[6][6],s[6][6]; 
+int p[5]; 
+  
+ void MatrixChain(int *p, int n) { 
+ 	//p[i]为矩阵序列中矩阵[i]的行数==下一矩阵的列数
+	//P[n]为最后矩阵的列数  
+ 	//n==矩阵数，m记录计算数量，s记录断开位置  
+ 	 
+ 	for(int i=1; i<=n; ++i) m[i][i]=0;//单个矩阵计算次数为0  
+ 	for(int r=2; r<=n; ++r) {         // 每一斜行  
+ 		for(int i=1;i <=n-r+1; ++i) {  //行开始  
+ 			int j=i+r-1;		       //行结束  
+ 			m[i][j]=m[i+1][j]+p[i-1]*p[i]*p[j]; 
+ 			s[i][j]=i;				   //在A[i:j]之间在s[i][j]开断  
+ 			for(int k=i+1; k<j; ++k) { 
+ 				//求解本次分组(含有j-i个元素的分组)中最小的计算次数  
+ 				int t=m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j]; 
+ 				if(t<m[i][j]) { 
+ 					m[i][j] =t; 
+ 					s[i][j] = k;//加括号位置  
+ 				} 
+ 			} 
+ 		} 
+ 	} 
+}
+
 int main(int argc,char*argv[]) {
 	p[0]=10;	p[1]=20;	p[2]=10;	p[3]=30;
 	p[4]=40;
@@ -71,3 +104,66 @@ int main(int argc,char*argv[]) {
 	Traceback(1,5);
 	return 0;
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+void MatrixChain_Traceback(int i,int j,int n,int *ss,int *rec) 
+{
+	//i,j :in子链开始及结束位置
+	//n :in 矩阵的数量 
+	//ss[] :in子链最优断开位置 
+	//rec[] :out 每个矩阵左侧'('的数量以及右侧')'的数量 
+	
+	if(i==j) return ;
+	int k=ss[i*(n+1)+j];
+	MatrixChain_Traceback(i,k,n,ss,rec);
+	MatrixChain_Traceback(k+1,j,n,ss,rec);
+	if(i!=k) {
+		rec[i+i]++;
+		rec[k+k+1]++;	
+	} 
+	if(k+1!=j) {
+		rec[k+k+2]++;
+		rec[j+j+1]++;	
+	}
+}
+
+void PrintResult(int n,int *rec) 
+{
+	//n :in 矩阵的数量 
+	//rec[] :out 每个矩阵左侧'('的数量以及右侧')'的数量 
+	
+	for(int i=1;i<=n;++i) {
+		for(int k=0;k<rec[i+i];k++) 
+			cout<<'(';
+		cout<<"A"<<i;
+		for(int k=0;k<rec[i+i+1];k++) 
+			cout<<')';
+	}
+}
+
+
+int main0() 
+{
+	//int *s = new int[n+1][n+1];
+	p[0]=10;	p[1]=20;	p[2]=10;	p[3]=30; 
+ 	p[4]=40; 
+ 	for(int i=0; i<=n; ++i) { 
+ 		for(int j=0; j<=n; ++j) { 
+ 			m[i][j]=0; 
+ 			s[i][j]=0; 
+ 		}
+ 	}
+ 	
+	MatrixChain(p,n);
+	int *rec  = new int[n+n+2];
+	for(int i=2;i<=n+n+1;i++) rec[i] = 0;
+	MatrixChain_Traceback(1,n,n,reinterpret_cast<int*>(s),rec);
+	PrintResult(n,rec);
+	cin.get();
+	delete[] rec;
+	return 0;
+}
+//////////////////////////////////////////////////////////////////////////////////////
+
--- MaxSumOfSubsegment.cpp
+++ MaxSumOfSubsegment.cpp
@@ -26,26 +26,73 @@ int MaxSum(int n,int a[],int &start,int &end)
 	return sum;
 }
 
-int MaxSum(int n,int a[]) 
+int MaxSubSum(int *a,int left,int right) 
 {
-	//动态规划求最大子段和 
-	int sum = 0,b= 0;
-	for(int i=0;i<n;++i) {
-		if(b>0) b+=a[i];
-		else b = a[i];
-		if(b>sum) sum = b;
-	}
+	//分治法 
+	int sum = 0;
+	if(left == right) sum = a[left]>0?a[left]:0;
+	else {
+		int center =(left+right) / 2;
+		int leftsum = MaxSubSum(a,left,center);
+		int rightsum = MaxSubSum(a,center+1,right);
+		int s1 = 0;
+		int lefts = 0;
+		for(int i=center;i>=left;i--) {
+			lefts +=a[i];
+			if(lefts>s1) s1 = lefts;
+		}
+		int s2 = 0;
+		int rights = 0;
+		for(int i=center+1;i<=rights;++i) {
+			rights+=a[i];
+			if(rights>s2) s2=right;
+		}
+		sum = s1+s2;
+		if(sum<leftsum) sum = leftsum;
+		if(sum<rightsum) sum = rightsum;
+	} 
 	return sum;
-} 
+}
 
+int MaxSubSum(int n,int *a,int &istart,int &iend) 
+{
+	//n元素数
+	//a求解最大字段和的数组 
+	//start end最大字段和的起始与终止位置
+	//返回最大字段和 
+	int b[n+1];//b[i]是a[i]结尾的所有字段和的最大值 
+	int d[n+1];
+	
+	int sum = 0;
+	b[0]=0;
+	for(int i=1;i<=n;i++) {
+		if(b[i-1]>0) {
+			b[i]=b[i-1]+a[i];
+			d[i]=1;
+		} else {
+			b[i]=a[i];
+			d[i]=2;
+		}
+		if(sum<b[i]) {
+			sum = b[i];
+			iend = i;
+		}
+	}
+	istart =iend;
+	while(istart>0&&d[istart]==1) istart--;
+	return sum;
+}
 
 int main() 
 {
-	int a[] = {1,2,-11,15,6,-5,-6,8,10,-12};
-	int start,end;
-	cout<<MaxSum(sizeof(a)/sizeof(int),a,start,end)<<endl;
-	cout<<start<<' '<<end<<endl;
-	cout<<MaxSum(sizeof(a)/sizeof(int),a)<<endl;
-	getchar(); 
+	
+	int a[] = {0,-1,2,3,45,-34,15,33,-24};
+	int i,j;
+	cout<<"最大字段和："<<MaxSubSum(sizeof(a)/sizeof(int)-1,a,i,j)<<endl;
+	cout<<"最大字段从"<<i<<"到"<<j<<endl; 
+	cin.get();
 	return 0;
-}
+} 
+
+
+
